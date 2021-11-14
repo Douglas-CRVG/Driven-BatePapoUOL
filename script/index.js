f6a4d2b7958ca01b83d3529684ddab12126920d0
Douglas <douglas.07.nascimento@gmail.com>
2021-11-13 21:15:34 -0300

    layout correction and scroll with each new message

--- script/index.js
+++ script/index.js
@@ -3,19 +3,22 @@ const URL_API = "https://mock-api.driven.com.br/api/v4/uol";
 //status
 //messages
 let user = {};
+let lastMessage;
+let penultimateMessage;
 
+setInterval(fetchMessages, 3000);
 
 setInterval(() => {
     axios.post(URL_API + "/status", user);
 }, 5000);
 
-function fetchMessages(){
+function fetchMessages() {
     axios.get(URL_API + "/messages").then(renderMessages);
-    setInterval(fetchMessages, 3000);
 }
 
-function renderMessages(response){
+function renderMessages(response) {
     const props = response.data;
+    penultimateMessage = lastMessage;
     const containerMessages = document.querySelector("main");
     containerMessages.innerHTML = "";
     props.map(prop => {
@@ -27,16 +30,16 @@ function renderMessages(response){
             time
         } = prop;
 
-        if(type === "status"){
-            containerMessages.innerHTML +=`
+        if (type === "status") {
+            containerMessages.innerHTML += `
             <div class="status" data-identifier="message">
                 <p>
                     <span>(${time})  </span><strong>${from} </strong>${text}
                 </p>
             </div>
             `;
-        } else if(type === "private_message" && (from === user.name || to === user.name)){
-            containerMessages.innerHTML +=`
+        } else if (type === "private_message" && (from === user.name || to === user.name)) {
+            containerMessages.innerHTML += `
             <div class="private" data-identifier="message">
                 <p>
                     <span>(${time})  </span><strong>${from}</strong> reservadamente para <strong>${to}: </strong>${text}
@@ -44,20 +47,24 @@ function renderMessages(response){
             </div>
             `;
         } else {
-            containerMessages.innerHTML +=`
+            containerMessages.innerHTML += `
             <div class="message" data-identifier="message">
                 <p>
                     <span>(${time})  </span><strong>${from}</strong> para <strong>${to}:  </strong>${text}
                 </p>
             </div>
             `;
-        } 
+        }
     });
+    lastMessage = document.querySelector("main div:last-child");
+    if (lastMessage.innerHTML !== penultimateMessage.innerHTML){
+        lastMessage.scrollIntoView();
+    }
 }
 
-function enterRoom(){
-    user = { name: prompt ("Digite o nome de usuário:") };
-    if (user.name === "" ){
+function enterRoom() {
+    user = { name: prompt("Digite o nome de usuário:") };
+    if (user.name === "") {
         alert("Insira um nome válido!");
         enterRoom();
     }
